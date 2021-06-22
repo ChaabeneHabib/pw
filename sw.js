c7dcdf262fdb73be78875dea0ffe1a71bf4ddf86
Habib CHAABENE <habib.chaabene@esprit.tn>
2021-06-22 10:28:54 +0100

    fix 9

--- sw.js
+++ sw.js
@@ -3,4 +3,4 @@ self.addEventListener('notificationclick', function(event) {
   // here data you access from event using event.notification.data
    console.log('On notification click: ', event.notification.tag);
  
- }
+ });
