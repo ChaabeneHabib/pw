85e64ce8fa0a1f84e3f56b7729655ad28c327d51
Habib CHAABENE <habib.chaabene@esprit.tn>
2021-06-22 10:05:08 +0100

    fix 1

--- push.js
+++ push.js
@@ -7,11 +7,11 @@ let vapikeys = {
 push.setVapidDetails('mailto:habib.chaabene@esprit.tn', vapikeys.publicKey, vapikeys.privateKey)
 
 let sub = {
-   endpoint: "https://fcm.googleapis.com/fcm/send/eqtqLPwZGhY:APA91bFzTqIdeMxrYPQ970NllnLWTXYT5gh8LZ5h7uIJmIireYpCsYMyalAQJGISEShadMWK1k41AevUqNcxK0DLx8g39x-XKwl7z0AuVlPxPW5DWxmirNP6_P0S4EfMWRvi8UOSWfQ3",
-   expirationTime: null,
-   keys: {
-      p256dh: "BLuTUuhv_HYuVV3JhSxcVv1gTBWN5ZIv-qw2XoYoH0EC7iOaeh34LNEGO7YydVqIagFj-zXznAyaWveoEyoQPKM",
-      auth: "ndpFqIWYtHP-5Z_IEj_Qvw"
-   }  
-};
+   "endpoint": "https://fcm.googleapis.com/fcm/send/eqtqLPwZGhY:APA91bFzTqIdeMxrYPQ970NllnLWTXYT5gh8LZ5h7uIJmIireYpCsYMyalAQJGISEShadMWK1k41AevUqNcxK0DLx8g39x-XKwl7z0AuVlPxPW5DWxmirNP6_P0S4EfMWRvi8UOSWfQ3",
+   "expirationTime": null,
+   "keys": {
+      "p256dh": "BLuTUuhv_HYuVV3JhSxcVv1gTBWN5ZIv-qw2XoYoH0EC7iOaeh34LNEGO7YydVqIagFj-zXznAyaWveoEyoQPKM",
+      "auth": "ndpFqIWYtHP-5Z_IEj_Qvw"
+   }
+}
 push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,20 +1,4 @@
-self.addEventListener('push',(e) => {
-   var options = {
-      body : 'this notification was generated from a push!',
-      icon :'images/example.png',
-      vibrate:[100,50,100],
-      data : {
-         dateOfArrival : Date.now() , 
-         primaryKey : '2'
-      },
-      actions : [{
-         action : 'explore',
-         title : 'Explore this new world ',
-         icon : 'images/checkmark.png'
-      },
-   { action : 'close' , title :'Close' , icon : 'images/xmark.png'}
-]
-   };
-  e.waitIntil(
-   self.registration.sendNotification('Hello Linedata ', options));
-}); 
+self.addEventListener('push', function(event) {
+   const promiseChain = self.registration.showNotification('Hello, World.');
+   event.waitUntil(promiseChain);
+ });
