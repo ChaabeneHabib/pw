a802b6cb3e0a5fe5c002db587be1d4341f4920a0
Habib CHAABENE <habib.chaabene@esprit.tn>
2021-06-22 10:15:12 +0100

    fix 4

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-self.addEventListener('push', function(event) {
-  event.waitUntil( runtime.register().then(registration => {
-    registration.showNotification("Hello, World");
-}))
- });
+self.addEventListener('push',(e) => {
+   var options = {
+      body : 'this notification was generated from a push!',
+      icon :'images/example.png',
+      vibrate:[100,50,100],
+      data : {
+         dateOfArrival : Date.now() , 
+         primaryKey : '2'
+      },
+      actions : [{
+         action : 'explore',
+         title : 'Explore this new world ',
+         icon : 'images/checkmark.png'
+      },
+   { action : 'close' , title :'Close' , icon : 'images/xmark.png'}
+]
+   };
+  e.waitIntil(
+   self.registration.sendNotification('Hello Linedata ', options));
+}); 
